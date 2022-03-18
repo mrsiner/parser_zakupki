7057356e3e0051e621b2e47e9695303fe7338318
mrsiner <deliseev19@gmail.com>
2022-03-18 17:48:13 +0300

    db file

--- db.py
+++ db.py
@@ -1,3 +1,17 @@
 from aiogram.types import ParseMode
 from aiogram import utils
-import peeweee
+from peeweee import *
+
+db = SqliteDatabase('tender.db')
+
+class BaseModel():
+    class Meta:
+        database = db
+
+
+
+class Bitrix_FZ_44(BaseModel):
+    title = CharField()
+    url = TextField()
+
+
